d8176c90087553a1b84358497d5e9f955eb55d87
mikrodinet <116235742+mikrodinet@users.noreply.github.com>
2023-05-12 10:03:39 +0700

    Update MainActivity.java

--- app/src/main/java/eu/org/mikrodinet/MainActivity.java
+++ app/src/main/java/eu/org/mikrodinet/MainActivity.java
@@ -86,4 +86,66 @@ public class MainActivity extends Activity {
         }
     }
 
+
+//initialize the goodies
+function initAd(){
+        if ( window.plugins && window.plugins.AdMob ) {
+            var ad_units = {
+                ios : {
+                    banner: 'ca-app-pub-6827901881006590/7982089965',		//PUT ADMOB ADCODE HERE
+                    interstitial: 'ca-app-pub-6827901881006590/6698817209'	//PUT ADMOB ADCODE HERE
+                },
+                android : {
+                    banner: 'ca-app-pub-6827901881006590/7982089965',		//PUT ADMOB ADCODE HERE
+                    interstitial: 'ca-app-pub-6827901881006590/6698817209'	//PUT ADMOB ADCODE HERE
+                }
+            };
+            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+
+            window.plugins.AdMob.setOptions( {
+                publisherId: admobid.banner,
+                interstitialAdId: admobid.interstitial,
+                bannerAtTop: false, // set to true, to put banner at top
+                overlap: true, // set to true, to allow banner overlap webview
+                offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+                isTesting: false, // receiving test ad
+                autoShow: false // auto show interstitial ad when loaded
+            });
+
+            registerAdEvents();
+            window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown
+            window.plugins.AdMob.requestInterstitialAd();
+
+        } else {
+            //alert( 'admob plugin not ready' );
+        }
+}
+//functions to allow you to know when ads are shown, etc.
+function registerAdEvents() {
+        document.addEventListener('onReceiveAd', function(){});
+        document.addEventListener('onFailedToReceiveAd', function(data){});
+        document.addEventListener('onPresentAd', function(){});
+        document.addEventListener('onDismissAd', function(){ });
+        document.addEventListener('onLeaveToAd', function(){ });
+        document.addEventListener('onReceiveInterstitialAd', function(){ });
+        document.addEventListener('onPresentInterstitialAd', function(){ });
+        document.addEventListener('onDismissInterstitialAd', function(){
+        	window.plugins.AdMob.createInterstitialView();			//REMOVE THESE 2 LINES IF USING AUTOSHOW
+            window.plugins.AdMob.requestInterstitialAd();			//get the next one ready only after the current one is closed
+        });
+    }
+    
+    //display the banner
+    function showBannerFunc(){
+    window.plugins.AdMob.createBannerView();
+    }
+    //display the interstitial
+    function showInterstitialFunc(){
+    window.plugins.AdMob.createInterstitialView();	//get the interstitials ready to be shown and show when it's loaded.
+    window.plugins.AdMob.requestInterstitialAd();
+    }
+    
+    window.plugins.AdMob.destroyBannerView();
+    }
+
 }
