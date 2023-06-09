fc2f0fa2f4d2c3cc7849d5aa004531de1bfeb801
mikrodinet <116235742+mikrodinet@users.noreply.github.com>
2023-06-09 21:44:58 +0700

    Create CordovaEventBuilderTest.java

---
+++ admob/test/android/CordovaEventBuilderTest.java
@@ -0,0 +1,45 @@
+package eu.org.mikrodinet.app.cordova.admob;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class CordovaEventBuilderTest {
+    @Test
+    public void buildEvent() {
+        String js = new CordovaEventBuilder("event").build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event');");
+    }
+
+    @Test
+    public void ignoreEmptyData() {
+        String js = new CordovaEventBuilder("event").withData("").build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event');");
+    }
+
+    @Test
+    public void buildEventWithData() {
+        String js = new CordovaEventBuilder("event").withData("{}").build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event',{});");
+    }
+
+    @Test
+    public void buildEventWithJSONObject() throws JSONException {
+        JSONObject jsonObj = new JSONObject("{\"data\": 1}");
+        assertEquals(jsonObj.toString(), "{\"data\":1}");
+        CordovaEventBuilder builder = new CordovaEventBuilder("event").withData(jsonObj);
+        String js = builder.build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event',{\"data\":1});");
+
+
+        jsonObj = new JSONObject();
+        assertEquals(jsonObj.toString(), "{}");
+        js = builder.withData(jsonObj).build();
+        assertEquals(js, "javascript:cordova.fireDocumentEvent('event',{});");
+    }
+}
